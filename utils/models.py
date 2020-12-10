dc34bb12704e9d83f2156c2bc48e41eb3dfc7f07
Gabriel <gpestre@users.noreply.github.com>
2020-12-10 17:26:43 -0500

    Add documentation.

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,47 @@
+"""
+Implementations of Bayesian models.
+
+For neural networks, we describe the dimensions
+with the following conventions, unless otherwise noted:
+
+- The input X is generally a dataset with N rows and M features.
+  In some cases, we may have a "stack" of R datasets
+  (i.e. X may be a R-by-N-by-M tensor instead of the usual N-by-M matrix)
+  but this case is not supported by our initial implementations.
+
+- The output Y is generally an N-by-K matrix (for a K-output model)
+  but may also be R-by-N-by-K, analogously with the input X.
+
+- The weights W of a single neural network are stored
+  as a 1-by-D matrix (for a network with D weights).
+  When representing the weights of a set of S models
+  (e.g. mutiple samples from a posterior), the weights
+  may be stored in an S-by-D matrix. (Additionally,
+  our implementation has an internal representation
+  that uses a list of weights for each layer, but
+  that version need not be exposed to the user.)
+
+- In a latent variable model, the X and Y inputs follow the same
+  conventions as above, but L latent features of Gaussian noise
+  are appeneded to the input X to form an augmented input X'.
+  Typically there is a single latent variable (i.e. L=1),
+  but we have made our implementation flexible so that we can
+  exeperiment with multiple latent variables.
+  Additionally, Gaussian noise is added to each of the output Y
+  (potentially with a different variance for each of the K outputs),
+  to form a pertured output Y'.
+
+In summary:
+- R: number of "stacked" datasets for X and Y (and X' and Y').
+- N: number of data points in X and Y (and X' and Y').
+- M: number of features in X.
+- K: number of outputs in Y.
+- S: number of different models in W.
+- D: number of weights in each model.
+- L: number of latent noise inputs.
+"""
+
+
 from autograd import numpy as np
 from autograd import grad
 from autograd.misc.optimizers import adam
